21eac396c711b62183e8622d5014b62e98536f00
Artem <artknutov@gmail.com>
2022-06-04 23:54:52 +0300

    Third commit

--- src/com.artem.lessons/Greetings.java
+++ src/com.artem.lessons/Greetings.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Greetings {
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
--- src/com.artem.lessons/SortArray.java
+++ src/com.artem.lessons/SortArray.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class SortArray {
+import java.util.Random;
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        Random rnd = new Random();
+
+        //random numbers input
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rnd.nextInt(100);
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        //bubblesort
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = arr.length - 1; j > i; j--) {
+                if (arr[j - 1] > arr[j]) {
+                        int temp  = arr[j-1];
+                        arr[j-1] = arr[j];
+                        arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
 }
