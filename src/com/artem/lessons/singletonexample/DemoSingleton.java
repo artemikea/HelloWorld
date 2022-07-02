8f6a64545aa5682cf97e8b9225d5b3416e85e5df
Artem <artknutov@gmail.com>
2022-07-02 21:10:55 +0300

    singleton example

--- src/com/artem/lessons/singletonexample/DemoSingleton.java
+++ src/com/artem/lessons/singletonexample/DemoSingleton.java
@@ -2,8 +2,10 @@ package com.artem.lessons.singletonexample;
 
 public class DemoSingleton {
     public static void main(String[] args) {
-        Singleton singleton = Singleton.getInstance("ha");
-        Singleton anotherSingleton = Singleton.getInstance("haha");
+        Singleton singleton = Singleton.getInstance();
+        Singleton anotherSingleton = Singleton.getInstance();
+        singleton.setValue("ha");
+        anotherSingleton.setValue("haha");
         System.out.println(singleton.value);
         System.out.println(anotherSingleton.value);
     }
--- src/com/artem/lessons/singletonexample/Singleton.java
+++ src/com/artem/lessons/singletonexample/Singleton.java
@@ -4,14 +4,16 @@ public class Singleton {
     public String value;
     private static Singleton INSTANCE = null;
 
-    private Singleton(String value) {
-        this.value = value;
-    }
+    private Singleton() {}
 
-    public static Singleton getInstance(String value) {
+    public static Singleton getInstance() {
         if(INSTANCE == null) {
-            INSTANCE = new Singleton(value);
+            INSTANCE = new Singleton();
         }
         return INSTANCE;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
     }
 }
